009bbf1dad522d8697754d637eacdb3e37a4ad10
Võ Nguyễn Minh Thư <vonguyenminhthu43@gmail.com>
2022-04-06 19:37:35 +0700

    create home view

--- BookShop/BookShop/urls.py
+++ BookShop/BookShop/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from homepage import views
 
 '''link from homepage.urls to here using include'''
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('homepage.urls'))
+    path('',views.homepage_view, name='home'),
+    path('home/',views.homepage_view, name='home'),
+    path('admin/', admin.site.urls)
 
 ]
+
  
